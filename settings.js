f6dbf0ef56e6b16869d73f03f9962cc6fa61e269
Sanduntharaka00t <160314515+Sanduntharaka00t@users.noreply.github.com>
2024-07-11 18:43:30 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,13 +8,13 @@ function convertToBool(text, fault = 'true') {
 global.thumb = fs.readFileSync('./lib/bugs/venom.jpg')
 const BOTNAME = 'VAJIRA-MD|-WA-BOT™';
 const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
-global.owner = process.env.OWNER_NUMBER  || '94701992015' ;  // ADD YOUR NUMBER WITHOUT +
+global.owner = process.env.OWNER_NUMBER  || '94728756898' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'youre session id' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=XAkS3KjZ#gMH-zK9O6Gsr8GQjOUqO6O27ljCrF_ujco8Go0WvlRw' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
-MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
-MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
+MODERATORS : process.env.MODERATORS === undefined ? "94764544760,94754015815" : process.env.MODERATORS ,    
+MAX_SIZE: process.env.MAX_SIZE === undefined ? '1200': process.env.MAX_SIZE,/*add this in megabytes*/    
 OWNER_NAME: process.env.OWNER_NAME || 'KAVINDA' ,
 COMMAND_TYPE: process.env.COMMAND_TYPE || 'nonbutton' ,
 LOGO: process.env.LOGO || `https://telegra.ph/file/ab0ea6ad90e2eb98090d9.jpg` ,
